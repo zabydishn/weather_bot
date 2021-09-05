9f2673ed25089308712fdd2f5830a46d5e5129f5
zabydishn <88948305+zabydishn@users.noreply.github.com>
2021-09-05 21:20:55 +0300

    Initial commit

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "e551fc4e67759863128b63e288fd2e8f"
-tg_bot_token = "1967081919:AAFT845pUCzDnibOS8ac4kG4F7UGx9DSe9o"
+open_weather_token = "your open weather token"
+tg_bot_token = "your API token"
